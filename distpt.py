2bfb3c4b22db52905fd72c28ab3fda25fe58b975
Loriano Storchi <loriano@storchi.org>
2019-09-16 20:34:16 +0200

    last

--- distpt.py
+++ distpt.py
@@ -29,17 +29,22 @@ def main ():
     line  = fp.readline()
     xs, ys, zs = line.split()
     pp = point(float(xs), float(ys), float(zs))
+    totdist = 0.0
     for line in fp:
         xs, ys, zs = line.split()
 
+        #print (pp.getx(), pp.gety(), pp.getz())
         ap = point(float(xs), float(ys), float(zs))
-        print (ap.getx(), ap.gety(), ap.getz())
-        print (ap.distfrom(pp.getx(), pp.gety(), pp.getz()))
-         
-        pp = ap
-        print (pp.getx(), pp.gety(), pp.getz()) 
+        dist = ap.distfrom(pp.getx(), pp.gety(), pp.getz())
+        pp = point(ap.getx(), ap.gety(), ap.getz())
+        #print (pp.getx(), pp.gety(), pp.getz()) 
+        #print (dist)
+
+        totdist += dist
 
     fp.close()
+
+    print ("Total dist: %10.5f "%totdist)
 
 
 if __name__== "__main__":
